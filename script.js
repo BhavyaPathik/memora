c0f98efddb3b6ad17687e78778df69be1919ab52
Bhavya Pathik <bhavvy2006@gmail.com>
2025-09-22 20:53:05 +0530

    js updates 1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementsByClassName("scrolltop")[0].addEventListener("click", function(){
     window.scrollTo({top:0, behavior: 'smooth'});
 });
+alert("DO you WISH to Continue this site might be too HILARIOUS!!");
